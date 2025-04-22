c3f3972b9c1dd7078b243d7fc8983d4b26b634b7
Yvette IRADUKUNDA <iradyvette98@gmail.com>
2025-04-22 19:57:29 +0200

    Adding Changes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> students;
-        students = new HashMap<>();
-        students.put(1, "John");
-        students.put(2, "Jane");
-        students.put(3, "Bob");
-        students.put(4, "Mary");
+        Map<Integer, String> studentsMap = new HashMap<>();
+        studentsMap.put(4, "John");
+        studentsMap.put(2, "Jane");
+        studentsMap.put(3, "Bob");
+        studentsMap.put(1, "Mary");
 
+
+        System.out.println("unsorted students: " + studentsMap);
+
+        Map<Integer, String> sortedStudentsMap = new TreeMap<>(studentsMap);
+        System.out.println("Sorted Students: " + sortedStudentsMap);
+
+
+        //sort by value
+        List<Map.Entry<Integer, String>> list = new ArrayList<>(studentsMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Map<Integer, String> sortedByValueMap = new LinkedHashMap<>();
+        for (Map.Entry<Integer, String> entry : list) {
+            sortedByValueMap.put(entry.getKey(), entry.getValue());
+        }
+        System.out.println("Sorted Student Map by Value: " + sortedByValueMap);
     }
 }
